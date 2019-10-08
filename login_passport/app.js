8346d75ee2bf2f279ff361b7e1f4b6628a6230d9
Josias Calebe <josiascalebe@gmail.com>
2019-10-07 22:29:03 -0300

    initial commit

---
+++ login_passport/app.js
@@ -0,0 +1,19 @@
+ //mongodb+srv://<username>:<password>@omnistack-5fdrq.mongodb.net/admin?retryWrites=true&w=majority
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//Router
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server aberto na porta ${PORT}`));
+
+
+
