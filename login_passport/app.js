e31e06cd031d920f49bb00b9c0ca32cccabf112b
Josias Calebe <josiascalebe@gmail.com>
2019-10-10 18:50:25 -0300

    MongoDB Connected

--- login_passport/app.js
+++ login_passport/app.js
@@ -1,7 +1,16 @@
  //mongodb+srv://<username>:<password>@omnistack-5fdrq.mongodb.net/admin?retryWrites=true&w=majority
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
 const app = express();
+
+//Db Config
+const db = require('./config/keys').MongoURI;
+
+mongoose.connect(db, { useNewUrlParser: true})
+.then(() => console.log('MongoDB Connected...'))
+.catch( err => console.log(err));
 
 //EJS
 app.use(expressLayouts);
@@ -14,6 +23,3 @@ app.use('/users', require('./routes/users'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server aberto na porta ${PORT}`));
-
-
-
---
+++ login_passport/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI:'mongodb+srv://calebe:cal3b3@omnistack-5fdrq.mongodb.net/test?retryWrites=true&w=majority'
+}
